5a06fd19629b6ff9882dcf06498d43dfc60dfd0a
mwtilton <charlietoast04@gmail.com>
2020-07-04 23:30:33 -0700

    Adding in changes for output folder arg

--- asnlookup.py
+++ asnlookup.py
@@ -20,14 +20,14 @@ def banner():
         |__| [__  |\ | |    |  | |  | |_/  |  | |__]
         |  | ___] | \| |___ |__| |__| | \_ |__| |
 
-         Author: Yassine Aboukir (@yassineaboukir)\n''')
+         Author: Yassine Aboukir (@yassineaboukir)\n
+         Python3: mwtilton\n''')
 
 def parse_args():
     # parse the argument
     parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + " -o twitter")
     org = parser.add_argument('-o', '--org', help="Organization to look up", required=True)
-    nmapscan = parser.add_argument('-n', '--nmapscan', help="Run Nmap", required=False, action="store", nargs='?', const="-p 1-65535 -T4 -A -v")
-    masscan = parser.add_argument('-m', '--masscan', help="Run Masscan", required=False, action="store", nargs='?', const="-p0-65535 --rate 200")
+    output = parser.add_argument('--output', help="Output path (optional)", required=False)
     return parser.parse_args()
 
 def check_licensekey():
@@ -46,18 +46,21 @@ def check_licensekey():
             print (e)
             sys.exit(1)
 
+org = parse_args().org
+output_path = parse_args().output
+
 def download_db():
     global input
     useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0'
     # Download a local copy of ASN database from maxmind.com
     if (os.path.isfile('./GeoLite2-ASN-Blocks-IPv4.csv')) == False:
         print(colored("[*] Downloading ASN database ...\n", "red"))
-        os.system("wget -O GeoLite2-ASN-CSV.zip '{}' && unzip GeoLite2-ASN-CSV.zip && rm -f GeoLite2-ASN-CSV.zip && mv GeoLite*/* . && rm -f GeoLite2-ASN-Blocks-IPv6.csv && rm -f COPYRIGHT.txt LICENSE.txt && rm -rf GeoLite*/".format(download_link))
+        os.system("wget https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip && unzip GeoLite2-ASN-CSV.zip && rm -f GeoLite2-ASN-CSV.zip && mv GeoLite*/* . && rm -f GeoLite2-ASN-Blocks-IPv6.csv && rm -f COPYRIGHT.txt LICENSE.txt && rm -rf GeoLite*/")
         print(colored("\nDone!\n", "red"))
 
         # Extracting and saving database file size locally
         try:
-            response = requests.head("{}".format(download_link), headers={'User-Agent': useragent}, timeout = 10)
+            response = requests.get("https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip", headers={'User-Agent': useragent}, timeout = 10)
         except:
             print(colored("[*] Timed out while trying to connect to the database server, please run the tool again.", "red"))
             sys.exit(1)
@@ -67,7 +70,7 @@ def download_db():
     else:
         # Checking if there is a new database change and download a new copy if applicable
         try:
-            response = requests.head("{}".format(download_link), headers={'User-Agent': useragent}, timeout = 10)
+            response = requests.get("https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip", headers={'User-Agent': useragent}, timeout = 10)
         except:
             print(colored("[*] Timed out while trying to the database server, please run the tool again.", "red"))
             sys.exit(1)
@@ -79,14 +82,14 @@ def download_db():
                     print(colored("[*] It seems like you have not updated the database.","red"))
                     try: input = raw_input #fixes python 2.x and 3.x input keyword
                     except NameError: pass
-                    choice = input(colored("[?] Do you want to update now? [Y]es [N]o, default: [N] ", "red"))
+                    choice = input(colored("[?] Do you want to update now? [Y]es [N]o, default: [N] ", "red")) 
                     if choice.upper() == "Y":
                         os.system("rm -rf GeoLite2*")
                         print(colored("[*] Downloading a new copy of the database ...\n","red"))
-                        os.system("wget -O GeoLite2-ASN-CSV.zip '{} && unzip GeoLite2-ASN-CSV.zip && rm -f GeoLite2-ASN-CSV.zip && mv GeoLite*/* . && rm -f GeoLite2-ASN-Blocks-IPv6.csv  && rm -f COPYRIGHT.txt LICENSE.txt && rm -rf GeoLite*/".format(download_link))
+                        os.system("wget -O GeoLite2-ASN-CSV.zip https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip && unzip GeoLite2-ASN-CSV.zip && rm -f GeoLite2-ASN-CSV.zip && mv GeoLite*/* . && rm -f GeoLite2-ASN-Blocks-IPv6.csv  && rm -f COPYRIGHT.txt LICENSE.txt && rm -rf GeoLite*/")
 
                         try:
-                            response = requests.get("{}".format(download_link), headers={'User-Agent': useragent}, timeout = 10)
+                            response = requests.get("https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip", headers={'User-Agent': useragent}, timeout = 10)
                         except:
                             print(colored("[*] Timed out while trying to the database server, please run the tool again.", "red"))
                             sys.exit(1)
@@ -99,27 +102,34 @@ def extract_asn(organization):
     #read csv, and split on "," the line
     asn_ipv4 = csv.reader(open('GeoLite2-ASN-Blocks-IPv4.csv', "r"), delimiter=",")
     #loop through csv list
+    res = []    
     for row in asn_ipv4:
         #if current rows 2nd value is equal to input, print that row
-        if organization.upper().replace('_', ' ') in row[2].upper():
-            return(row[1])
+        if organization.upper() in row[2].upper():
+            res.append(row[1])
 
+    return list(set(res))
 
-def extract_ip(asn, organization):
+def extract_ip(asns, organization, output_path=None):
 
-    path_ipv6 = os.path.dirname(os.path.realpath(__file__)) + "/output/" + organization + "_ipv6.txt"
-    path_ipv4 = os.path.dirname(os.path.realpath(__file__)) + "/output/" + organization + "_ipv4.txt"
+    if not output_path:
+        output_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'output')
 
-    if asn:
-        if not os.path.exists("output"):
-            os.makedirs("output")
-        elif os.path.isfile('./output/' + organization + '.txt') == True:
-            os.system('cd ./output/ && rm -f ' + organization + '.txt')
-        else:
-            pass
+    path_ipv6 = os.path.join(output_path, organization + "_ipv6.txt")
+    path_ipv4 = os.path.join(output_path, organization + "_ipv4.txt")
 
-        ipinfo = "https://ipinfo.io/"
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    # elif os.path.isfile('./output/' + organization + '.txt') == True:
+    #     os.system('cd ./output/ && rm -f ' + organization + '.txt')
+    # else:
+    #     pass
 
+    ipinfo = "https://ipinfo.io/"
+    ipv6 = []
+    ipv4 = []
+
+    for asn in asns:
         try:
             response = requests.get(ipinfo + "AS" + asn)
         except:
@@ -128,8 +138,6 @@ def extract_ip(asn, organization):
 
         html = response.content
         soup = BeautifulSoup(html, 'html.parser')
-        ipv6 = []
-        ipv4 = []
         for link in soup.find_all('a'):
             if asn in link.get('href'):
                 search_criteria = '/' + "AS" + asn + '/'
@@ -140,25 +148,25 @@ def extract_ip(asn, organization):
                     else: ipv4.append(ip)
                 else: pass
 
-        print(colored("[*] IP addresses owned by {} are the following (IPv4 or IPv6):".format(organization),"green"))
+    print(colored("[*] IP addresses owned by {} are the following (IPv4 or IPv6):".format(organization),"red"))
 
-        if ipv4:
-            print(colored("\n[*] IPv4 addresses saved to: ", "green"))
-            print(colored("{}\n".format(path_ipv4), "yellow"))
-            with open("./output/" + organization + "_ipv4.txt", "w") as dump:
-                for i in ipv4:
-                    dump.write(i + "\n")
-                    print(colored(i, "yellow"))
+    if ipv4:
+        print(colored("\n[*] IPv4 addresses saved to: ", "red"))
+        print(colored("{}\n".format(path_ipv4), "yellow"))
+        with open(path_ipv4, "w") as dump:
+            for i in ipv4:
+                dump.write(i + "\n")
+                print(colored(i, "yellow"))
 
-        if ipv6:
-            print(colored("\n[*] IPv6 addresses saved to: ", "green"))
-            print(colored("{}\n".format(path_ipv6), "yellow"))
-            with open("./output/" + organization + "_ipv6.txt", "w") as dump:
-                for i in ipv6:
-                    dump.write(i + "\n")
-                    print(colored(i, "yellow"))
-    else:
-        print(colored("[*] Sorry! We couldn't find the organization's ASN and IP addresses.", "red"))
+    if ipv6:
+        print(colored("\n[*] IPv6 addresses saved to: ", "red"))
+        print(colored("{}\n".format(path_ipv6), "yellow"))
+        with open(path_ipv6, "w") as dump:
+            for i in ipv6:
+                dump.write(i + "\n")
+                print(colored(i, "yellow"))
+    # else:
+    #     print(colored("[*] Sorry! We couldn't find the organization's ASN and IP addresses.", "red"))
 
 def scanning(n, m, organization):
     # Only allow one scanner choice
@@ -185,11 +193,11 @@ def scanning(n, m, organization):
 
 if __name__ == '__main__':
     banner()
-    nmapscan = parse_args().nmapscan
-    masscan = parse_args().masscan
     org = parse_args().org \
                         .replace(' ', '_')
     check_licensekey()
     download_db()
     extract_ip(extract_asn(org), org)
-    scanning(nmapscan, masscan, org)
+    
+    download_db()
+    extract_ip(extract_asn(org), org, output_path)
