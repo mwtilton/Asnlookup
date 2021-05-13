fef17446d15581d0bbc057bb2cd40b229c6eb119
mwtilton <charlietoast04@gmail.com>
2021-05-12 19:32:03 -0700

    Adding in new ASN lookup tool

--- asnlookup.py
+++ asnlookup.py
@@ -6,6 +6,7 @@ import argparse
 import requests
 import re
 import os
+import json
 
 from termcolor import colored
 from bs4 import BeautifulSoup
@@ -28,6 +29,7 @@ def parse_args():
     parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + " -o twitter")
     org = parser.add_argument('-o', '--org', help="Organization to look up", required=True)
     license = parser.add_argument('-l', '--license', help="License to use for maxmind.", required=True)
+    token = parser.add_argument('-t', '--token', help="Token to use for ipinfo.", required=True)
     output = parser.add_argument('--output', help="Output path (optional)", required=False, default=None)
     return parser.parse_args()
 
@@ -47,15 +49,15 @@ def check_licensekey(license_key):
             print (e)
             sys.exit(1)
 
-def download_db(download_link, org, useragent):
-    geolite_asn_zip_filepath = f'/tmp/{org}/GeoLite2-ASN-CSV.zip'
+def download_db(download_link, org, useragent, output_path):
+    geolite_asn_zip_filepath = f'{output_path}/GeoLite2-ASN-CSV.zip'
     #geolite_asn_filesize_filepath = f'/tmp/{org}/filesize.txt'
     
-    geolite_asn_ipv4_csv_filepath = f'/tmp/{org}/GeoLite2-ASN-Blocks-IPv4.csv'
+    geolite_asn_ipv4_csv_filepath = f'{output_path}/GeoLite2-ASN-Blocks-IPv4.csv'
     #geolite_asn_ipv6_csv_filepath = f'/tmp/{org}/GeoLite2-ASN-Blocks-IPv6.csv'
 
-    if (os.path.exists(f'/tmp/{org}') == False):
-        os.makedirs(f'/tmp/{org}')
+    if (os.path.exists(f'{output_path}') == False):
+        os.makedirs(f'{output_path}')
 
     # Download a local copy of ASN database from maxmind.com
     if (os.path.isfile(geolite_asn_ipv4_csv_filepath)) == False:
@@ -63,14 +65,15 @@ def download_db(download_link, org, useragent):
         os.system(f"wget -O {geolite_asn_zip_filepath} '{download_link}'") #&& unzip {geolite_asn_zip_filepath} && rm -f {geolite_asn_zip_filepath} && rm -f {geolite_asn_ipv6_csv_filepath} && rm -f COPYRIGHT.txt LICENSE.txt && rm -rf GeoLite*/")
         
         with ZipFile(geolite_asn_zip_filepath, 'r') as zip_ref:
-            zip_ref.extractall(f'/tmp/{org}')
+            zip_ref.extractall(f'{output_path}')
         
-        os.system(f"mv /tmp/{org}/Geo*/GeoLite2-ASN-Blocks-IPv4.csv {geolite_asn_ipv4_csv_filepath}")
+        os.system(f"mv {output_path}/Geo*/GeoLite2-ASN-Blocks-IPv4.csv {geolite_asn_ipv4_csv_filepath}")
+        
         print(colored("\nDone!\n", "red"))
 
-def extract_asn(organization):
+def extract_asn(organization, output_path):
     #read csv, and split on "," the line
-    asn_ipv4 = csv.reader(open(f'/tmp/{organization}/GeoLite2-ASN-Blocks-IPv4.csv', "r"), delimiter=",")
+    asn_ipv4 = csv.reader(open(f'{output_path}/GeoLite2-ASN-Blocks-IPv4.csv', "r"), delimiter=",")
     #loop through csv list
     for row in asn_ipv4:
         #if current rows 2nd value is equal to input, print that row
@@ -78,69 +81,105 @@ def extract_asn(organization):
             print(colored(f">> {organization}: {row[1]} {row[2]}", "yellow"))
             return(row[1])
 
-def extract_ip(asn, organization, output_path):
+def extract_ip(asn, organization, output_path, token):
 
-    path_ipv6 = os.path.join(f"{output_path}/ipv6.txt")
-    path_ipv4 = os.path.join(f"{output_path}/ipv4.txt")
+    # path_ipv6 = os.path.join(f"{output_path}/ipv6.txt")
+    # path_ipv4 = os.path.join(f"{output_path}/ipv4.txt")
 
-    ipinfo = "https://ipinfo.io/"
-
+    # ipinfo.io/[IP address]?token=
+    # https://ipinfo.io/AS5586/json?token=
+    # https://stat.ripe.net/data/as-overview/data.json?resource=AS3333
+    # ipinfo = f"https://ipinfo.io/AS{asn}/json?token={token}"
+    # ipinfo = f"https://stat.ripe.net/data/as-overview/data.json?resource=AS{asn}"
+    ipinfo = f"https://api.bgpview.io/asn/AS{asn}/prefixes"
+    # print(colored(f"[*] {ipinfo}", "yellow"))
     try:
-        response = requests.get(ipinfo + "AS" + asn)
+        # response = requests.get(ipinfo)
+        response = requests.get(ipinfo, headers={'accept': 'application/json'})
+
     except:
         print(colored("[*] Timed out while trying to the ASN lookup server, please run the tool again.", "red"))
         sys.exit(1)
+    
+    # print(response.json())
+    print(json.dumps(response.json(), sort_keys=True, indent=4))    
 
-    html = response.content
-    soup = BeautifulSoup(html, 'html.parser')
-    ipv6 = []
-    ipv4 = []
-    for link in soup.find_all('a'):
-        if asn in link.get('href'):
-            search_criteria = '/' + "AS" + asn + '/'
-            ip = re.sub(search_criteria, '', link.get('href'))
-            if "robtex" not in ip:
-                if ":" in ip:
-                    ipv6.append(ip)
-                else: ipv4.append(ip)
-            else: pass
+    # html = response.content
+    # if html is None:
+    #     print(colored("[*] no html content found.", "red"))
+    #     exit(1)
+    
+    # print(html)
+    # print(json.dumps(html, sort_keys=True, indent=4))    
+
+    # soup = BeautifulSoup(html, 'html.parser')
+    # if soup is None:
+    #     print(colored("[*] no soup content found in html.", "red"))
+    #     exit(1)
+
+    # print(json.dumps(soup, sort_keys=True, indent=4))    
+    
+    # print(soup)
+
+    # ipv4 = []
+    
+    # for link in soup.find_all('a'):
+    #     print(colored(link, "blue"))
+    #     print(colored(asn, "blue"))
+        
+    #     if asn in link.get('href'):
+    #         search_criteria = '/' + "AS" + asn + '/'
+    #         ip = re.sub(search_criteria, '', link.get('href'))
+            
+    #         print(colored(asn, "green"))
+
+    #         if "robtex" not in ip:
+    #             if ":" in ip:
+    #                 ipv6.append(ip)
+    #             else: 
+    #                 print(colored(ip, "green"))
+    #                 ipv4.append(ip)
+    #         else: 
+    #             pass
 
     print(colored("[*] IP addresses owned by {} are the following (IPv4 or IPv6):".format(organization),"green"))
 
-    if ipv4:
-        print(colored("\n[*] IPv4 addresses saved to: ", "green"))
-        print(colored("{}\n".format(path_ipv4), "yellow"))
-        with open(path_ipv4, "w") as dump:
-            for i in ipv4:
-                dump.write(i + "\n")
-                print(colored(i, "yellow"))
+    # if ipv4:
+    #     print(colored("\n[*] IPv4 addresses saved to: ", "green"))
+    #     print(colored("{}\n".format(path_ipv4), "yellow"))
+    #     with open(path_ipv4, "w") as dump:
+    #         for i in ipv4:
+    #             dump.write(i + "\n")
+    #             print(colored(i, "yellow"))
 
-    if ipv6:
-        print(colored("\n[*] IPv6 addresses saved to: ", "green"))
-        print(colored("{}\n".format(path_ipv6), "yellow"))
-        with open(path_ipv6, "w") as dump:
-            for i in ipv6:
-                dump.write(i + "\n")
-                print(colored(i, "yellow"))
+    # if ipv6:
+    #     print(colored("\n[*] IPv6 addresses saved to: ", "green"))
+    #     print(colored("{}\n".format(path_ipv6), "yellow"))
+    #     with open(path_ipv6, "w") as dump:
+    #         for i in ipv6:
+    #             dump.write(i + "\n")
+    #             print(colored(i, "yellow"))
 
 if __name__ == '__main__':
     banner()
     org = (parse_args().org).replace(' ', '_')
     license_key = parse_args().license
-    output_path = parse_args().output
+    token = parse_args().token
+    output_path = os.path.join(parse_args().output, org)
     
     if output_path is None:
         output_path = os.path.join(f'/tmp/{org}', 'output')
 
-    print("ASN Output Path: {output_path}")
+    print(f"ASN Output Path: {output_path}")
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0'
     download_link = 'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN-CSV&license_key={}&suffix=zip'.format(license_key)
 
+    print(f"Checking License Key...")
     check_licensekey(license_key)
     
-    download_db(download_link, org, useragent)
+    download_db(download_link, org, useragent, output_path)
 
-    extract_ip(extract_asn(org), org, output_path)
+    extract_ip(extract_asn(org, output_path), org, output_path, token)
